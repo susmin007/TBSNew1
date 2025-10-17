4325d764b439f5ec49161ac64787bfab7a123cb0
gokulkrishnakb <gokulkrishnakb070@gmail.com>
2025-10-17 20:18:57 +0530

    Updated build files and config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TBSNew1/', // ✅ Correct format for GitHub Pages
+  base: '/', // ✅ Correct format for GitHub Pages
 })
