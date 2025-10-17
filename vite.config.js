5118ac2319b1774af6d17d7f55dc8b0f276d63e4
Shinish Sasidharan <shinish@live.com>
2025-10-17 12:12:26 +0530

    Added

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 important! matches your repo name
+  base: '/TBSNew1/', // ✅ Correct format for GitHub Pages
 })
